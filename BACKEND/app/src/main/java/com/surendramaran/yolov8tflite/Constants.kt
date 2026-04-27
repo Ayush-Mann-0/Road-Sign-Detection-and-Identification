60d6e41166a6dc0dbd4f1b22735c6a918f3920c6
Ayush Mann <rcbass.official@gmail.com>
2026-04-28 00:41:41 +0530

    Delete BACKEND directory

--- BACKEND/app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.surendramaran.yolov8tflite
-
-object Constants {
-    const val MODEL_PATH = "model_float32.tflite"
-    val LABELS_PATH: String? = "label.txt" // provide your labels.txt file if the metadata not present in the model
-}
